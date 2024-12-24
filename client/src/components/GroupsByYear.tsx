1a149dc12c4c8860301eccfe0aff65358efc74a6
menachem <MS0556727820@GMAIL.COM>
2024-12-24 17:59:02 +0200

    ede

--- client/src/components/GroupsByYear.tsx
+++ client/src/components/GroupsByYear.tsx
@@ -1,13 +1,4 @@
-// Organizations that were active in a given year with total incidents
-// Address: api/relationships/groups-by-year/
-// Method: GET
-// Parameters: Year.
-// Description: Displays a list of organizations that were active in a given year with their number of incidents.
-// a. Filtering options:
-// i. Select a year - display the organizations by the number of incidents associated with it in descending order from the number of incidents
-// largest to smallest
-// ii. Select a list organization - display incidents from years ago
-// b. Display: Graphs with filtering options
+
 
 import { Button, Menu, MenuItem, Select } from "@mui/material";
 import { useEffect, useState } from "react";
