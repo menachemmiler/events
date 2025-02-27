14acfe71ee3f23724ae860aa92bdf13e78aeca8e
MenyMiler <miler.yesodot@gmail.com>
2025-02-27 09:21:19 +0200

    update css a lote

--- client/src/components/DeadliestAttackTypes.tsx
+++ client/src/components/DeadliestAttackTypes.tsx
@@ -1,94 +1,4 @@
-// import { Bar } from "react-chartjs-2";
-// import {
-//   Chart as ChartJS,
-//   CategoryScale,
-//   LinearScale,
-//   BarElement,
-//   Title,
-//   Tooltip,
-//   Legend,
-// } from "chart.js";
-// import { useEffect, useState } from "react";
-// import SelectAllTransferList from "./SelectAllTransferList";
-
-// ChartJS.register(
-//   CategoryScale,
-//   LinearScale,
-//   BarElement,
-//   Title,
-//   Tooltip,
-//   Legend
-// );
-
-// const DeadliestAttackTypes = () => {
-//   const [attackData, setAttackData] = useState<any[]>([]);
-//   const [selectedTypes, setSelectedTypes] = useState<any[]>([]);
-
-//   const fetchData = async () => {
-//     try {
-//       const response = await fetch(
-//         `${import.meta.env.VITE_BASE_URL}/api/analysis/deadliest-attack-types`
-//       );
-//       const data = await response.json();
-//       setAttackData(data.data);
-//       setSelectedTypes(data.data);
-//     } catch (error) {
-//       console.error("Error fetching data:", error);
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
-
-//   useEffect(() => {
-//     console.log({ attackData });
-//   }, [attackData]);
-
-//   const chartData = {
-//     labels: selectedTypes.map((item) => item._id), // סוגי התקפות
-//     datasets: [
-//       {
-//         label: "מספר הנפגעים",
-//         data: selectedTypes.map((item) => item.count), // מספר הנפגעים לכל סוג התקפה
-//         backgroundColor: `rgba(255, 99, 132, 0.5)`,
-//         borderColor: "rgba(255, 99, 132, 1)",
-//         // borderWidth: 1,
-//       },
-//     ],
-//   };
-
-//   const options = {
-//     responsive: true,
-//     scales: {
-//       y: {
-//         beginAtZero: true,
-//       },
-//     },
-//   };
-
-//   return (
-//     <div className="deadliestAttackTypes">
-//       <div className="selectTypes">
-//         <SelectAllTransferList
-//           leftItems={attackData}
-//           selectedTypes={selectedTypes}
-//           setSelectedTypes={setSelectedTypes}
-//         />
-//       </div>
-
-//       <div className="graf">
-//         <h1>גרף סוגי התקפות</h1>
-//         <Bar options={options} data={chartData} className="bar" />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default DeadliestAttackTypes;
-
-
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Autocomplete, TextField } from "@mui/material";
 import { Bar } from "react-chartjs-2";
 import {
@@ -101,7 +11,14 @@ import {
   Legend,
 } from "chart.js";
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const DeadliestAttackTypes = () => {
   const [attackData, setAttackData] = useState<any[]>([]);
